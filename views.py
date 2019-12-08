0b75da716a19a1c0102d66227763327c509967c5
Jose Ramiro Fuentes Lara <jr.fuenteslara@outlook.com>
2019-12-07 19:03:39 -0600

    Modificación mayor de la página en el LHD.

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from app import app
-
+from info import miembros
 from flask import render_template
 
 
@@ -15,3 +15,14 @@ def about():
     """URL for about section."""
 
     return render_template('about.html')
+
+@app.route('/estructura')
+def estructura():
+    """Descripcion de cada estructura"""
+
+@app.route('/members')
+def members():
+    """Apartado de miembroos"""
+    members = miembros
+
+    return render_template('members.html',miembros=members)
